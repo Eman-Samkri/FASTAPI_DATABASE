519d241b84b56373e7ebf0a81c7bb5e17237f154
Eman Samkri <Eman.Jameel.jed.k2@tuwaiq.edu.sa>
2022-05-08 22:30:19 +0300

    CRUD delet

--- main.py
+++ main.py
@@ -33,20 +33,27 @@ def create_new_city(request:Weather , db: Session = Depends(get_db)):
     return new_city
 
 #http Method Get /  view all the cities with the weather temperature
-@app.get("/city")
+@app.get("/weather")
 def all_cities_weather (db: Session = Depends(get_db)):
     cities_weather = db.query(models.Weather).all()
     return cities_weather
 
 
-'''
-
 #http Method GET with path parameters
-@app.get("/weather/{city_name}")
-def index(city_name : str):
-    return {"msg": f"the weather of {city_name} is {cities_weather}"}
+@app.get('/weather/{city_id}')
+def city_weather(city_id,db: Session = Depends(get_db)):
+    city = db.query(models.Weather).filter(models.Weather.id == city_id).first()
+    return city
 
 
+@app.delete("/city/delete/{city_id}")
+def delete_user(city_id,db: Session = Depends(get_db)):
+    db.query(models.Weather).filter(models.Weather.id == city_id).delete(synchronize_session=False)
+    db.commit()
+    return "city deleted"
+
+
+'''
 
 #http Method Put / update
 @app.put("/city/update/{index}", response_model=ResponseModel)
@@ -54,9 +61,5 @@ def update_city(weather:CityWeather, index:int):
     cities_weather[index] = weather
     return ResponseModel(msg="Update city weather successfully", content=cities_weather)
 
-@app.delete("/city/delete/{index}", response_model=ResponseModel)
-def delete_user(index:int):
-    del cities_weather[index]
-    return ResponseModel(msg=f"deleted city with id: {index}", content=cities_weather)
 
 '''
