5c2f4926fcce3f59ea3ca2583de12657a02c8736
Eman Samkri <Eman.Jameel.jed.k2@tuwaiq.edu.sa>
2022-05-08 21:15:41 +0300

    CRUD get and post

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Depends
 from weather import  models
-from weather.database import engine
+from weather.database import engine , SessionLocal
+from weather.schemas import ResponseModel,Weather
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 #http Method GET
 @app.get("/")
 def index():
     return {"msg":"This is the index , or the home page"}
 
-'''
-#http Method Get /  view all the cities with the weather temperature
-@app.get("/weather", response_model=ResponseModel)
-def get_cities_weather():
-    return ResponseModel(msg="All cities weather", content=cities_weather)
 
+#http Method Post / create
+@app.post("/city/create")
+def create_new_city(request:Weather , db: Session = Depends(get_db)):
+    new_city = models.Weather(city_name = request.city_name, temperature = request.temperature, 
+    pressure = request.pressure, humidity = request.humidity, description = request.description)
+    db.add(new_city)
+    db.commit()
+    db.refresh(new_city)
+    return new_city
+
+#http Method Get /  view all the cities with the weather temperature
+@app.get("/city")
+def all_cities_weather (db: Session = Depends(get_db)):
+    cities_weather = db.query(models.Weather).all()
+    return cities_weather
+
+
+'''
 
 #http Method GET with path parameters
 @app.get("/weather/{city_name}")
 def index(city_name : str):
     return {"msg": f"the weather of {city_name} is {cities_weather}"}
 
-
-#http Method Post / create
-@app.post("/city/create", response_model=ResponseModel)
-def create_new_city(weather : CityWeather ):
-    cities_weather.append(weather)
-    return ResponseModel(msg="Added new city with weather", content=cities_weather)
 
 
 #http Method Put / update
